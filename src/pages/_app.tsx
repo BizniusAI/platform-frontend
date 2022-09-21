ced75ea73d5fc5fe4edf6afb4307c24e77d66905
An-Ting Hsu <andyhsu10@gmail.com>
2022-09-21 17:03:18 +0800

    feat: firebase google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
-import '@/styles/globals.css'
+import { getAnalytics } from 'firebase/analytics'
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
+import { useEffect } from 'react'
+
+import '@/styles/globals.css'
+import app from '@/utils/firebase'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      getAnalytics(app)
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
 
